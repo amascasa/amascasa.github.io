74371e607c2ec828f2a97525c0eff3e81da72b18
MasCasa <142547767+amascasa@users.noreply.github.com>
2024-04-27 10:38:22 -0700

    Add files via upload

---
+++ manifest.php
@@ -0,0 +1,12 @@
+<?php header('Content-type: text/cache-manifest'); ?>
+CACHE MANIFEST
+# Testing cache manifest file V1
+CACHE:
+
+https://amascasa.github.io/index.html
+https://amascasa.github.io/app.js
+https://amascasa.github.io/sw.js
+https://amascasa.github.io/lightgold.jpg
+https://amascasa.github.io/lightblue.jpg
+
+
