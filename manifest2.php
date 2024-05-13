b492f88cd2d5044f289af9b628054f6c937e31da
MasCasa <142547767+amascasa@users.noreply.github.com>
2024-05-13 14:55:20 -0700

    Add files via upload

--- manifest2.php
+++ manifest2.php
@@ -3,9 +3,6 @@ CACHE MANIFEST
 # Testing cache manifest file V1
 CACHE:
 
-https://amascasa.github.io/indexHybrid.html
-https://amascasa.github.io/app.js
-https://amascasa.github.io/sw2.js
-
-
-
+https: //amascasa.github.io/indexHybrid.html
+https: //amascasa.github.io/app2.js
+https: //amascasa.github.io/sw2.js
